7d060346e7427172def3ab1425f92a96f5a8c95f
Nico Rithner <nicorithner@gmail.com>
2020-12-05 14:04:12 -0700

    Add expressGraphQL and route (app.use())

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql');
 
 const app = express();
+
+app.use('/graphql', expressGraphQL({
+  schema: schema,
+  graphiql: true
+}))
 
 app.listen(4000, () => {
   console.log('Sever is running on port 4000...');
