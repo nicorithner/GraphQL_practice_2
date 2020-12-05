4a813349c5aa1df7d0e601aeadc1800c5636f0d7
Nico Rithner <nicorithner@gmail.com>
2020-12-05 14:27:03 -0700

    Add hardcoded data

--- schema.js
+++ schema.js
@@ -7,6 +7,13 @@ const {
   GraphQLNonNull
 } = require('graphql');
 
+// Hardcoded data
+const customers = [
+  { id: '1', name: 'John Doe', email: 'jdoe@gmail.com', age: 35 },
+  { id: '2', name: 'Steve Smith', email: 'steve@gmail.com', age: 25 },
+  { id: '3', name: 'Sara Williams', email: 'sara@gmail.com', age: 32 },
+];
+
 //CustomerType
 const CustomerType= new GraphQLObjectType({
   name: 'Customer',
