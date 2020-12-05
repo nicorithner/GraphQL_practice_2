3870151d3c8d8805ecd5abc9abcf63c1208d4abf
Nico Rithner <nicorithner@gmail.com>
2020-12-05 14:48:19 -0700

    Replace expressGraphQL for { graphqlHTTP }

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const expressGraphQL = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema.js');
 
 const app = express();
 
-app.use('/graphql', expressGraphQL({
+app.use('/graphql', graphqlHTTP({
   schema: schema,
   graphiql: true
 }))
